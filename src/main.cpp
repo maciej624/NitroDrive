d2b29b098247240d723d5e67dcac2421add43239
maciej624 <macko2sv@gmail.com>
2026-06-05 22:50:59 +0200

    v1.0.2.1 - weryfikacja Milestone

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,25 @@
-#include "Game.hpp"
-int main() { return 0; }
+#include <SFML/Graphics.hpp>
+#include "Car.hpp"
+
+int main() {
+    //czy sfml dziala?
+    sf::RenderWindow window(sf::VideoMode(800, 600), "NitroDrive - Test Milestone v0.1");
+
+    // tworzymy auto
+    Car myCar;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        // w przyszlosci rysujemy auto
+
+        window.display();
+    }
+    return 0;
+}
